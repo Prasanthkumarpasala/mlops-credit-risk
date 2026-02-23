4799d6471cdf1aad634f887f4608150cce1cb194
Prasanthkumarpasala <pasalaprasanthkumar@gmail.com>
2026-02-23 11:57:27 +0530

    Refactor data loading test with dummy CSV
    Refactor test_data_loading to create a dummy CSV file and validate data loading from it.

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,12 +1,18 @@
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
+import pandas as pd
 from src.data_loader import load_data
 
-def test_data_loading():
-    data = load_data("data/raw/credit_data.csv")
-    assert data.shape[0] > 0
-    assert "loan_approved" in data.columns
+def test_data_loading(tmp_path):
+    # Create dummy CSV
+    dummy_file = tmp_path / "dummy.csv"
     
+    df = pd.DataFrame({
+        "feature1": [1, 2, 3],
+        "loan_approved": [0, 1, 0]
+    })
+    
+    df.to_csv(dummy_file, index=False)
+
+    data = load_data(dummy_file)
+
+    assert data.shape[0] == 3
+    assert "loan_approved" in data.columns
